2ccd6d1bbad7004083a0c777d92d9f77bc841b94
Qofy <148817944+Qofy@users.noreply.github.com>
2025-10-26 17:04:14 +0100

    feat: add Criteria component and enhance Overview layout with investment criteria sections

--- real_estate_invsetment_site/src/lib/component/Criteria.svelte
+++ real_estate_invsetment_site/src/lib/component/Criteria.svelte
@@ -1,4 +1,20 @@
 <script>
+import "$lib/css/app.css"
+import {TrendingUp} from "lucide-svelte"
 
+export let icon = TrendingUp;
+export let color = "#16703a";
+export let h2 = "Long-term Wealth Building";
+export let p = "Your bundle is the perfect building block for your long-term wealth accumulation – secure properties with stable appreciation over years.";
+export let bg = "#182c26";
 </script>
 
+<div class="border border-[#2c313a] roundered p-3 flex gap-4">
+  <div class="w-20 h-10 rounded-[50%] flex items-center justify-center" style="background:{bg}">
+    <svelte:component this={icon} color={color}/>
+  </div>
+  <div class="text-[.7rem]">
+    <h2 class="text-[.9rem]">{h2}</h2>
+    <p class="text-color">{p}</p>
+  </div>
+</div>
--- real_estate_invsetment_site/src/lib/component/Overview.svelte
+++ real_estate_invsetment_site/src/lib/component/Overview.svelte
@@ -2,6 +2,7 @@
   import { apartmentdata} from "$lib/data/apartment-data";
   import "../css/app.css"
   import { Home, Bed, CircleCheck,Square,Bath,Wallet } from "lucide-svelte"
+	import Criteria from "./Criteria.svelte";
 
   // Features to display
   const features = [
@@ -16,13 +17,13 @@
 
 <main class="flex flex-col gap-8">
   <!--Description-->
-  <div class="flex flex-col gap-4.5 border border-[#2c313a] p-6 roundered">
+  <div class="flex flex-col gap-4.5 border border-[#2c313a] p-6 roundered bg-[#181b20]">
     <h1 class="font-medium text-2xl">Proper Description</h1>
     <p class="text-gray-400">{apartmentdata[0].description}</p>
   </div>
 
   <!--Key Feautures-->
-  <div class="flex flex-col gap-10 border border-[#2c313a] p-6 roundered">
+  <div class="flex flex-col gap-10 border border-[#2c313a] p-6 roundered bg-[#181b20]">
     <h1 class="font-medium text-2xl">Key Features</h1>
     <div class="grid grid-cols-2 gap-6">
       {#each features as feature}
@@ -40,15 +41,41 @@
   </div>
 
   <!--Investment Criteria-->
-  <div>
+  <div class="bg-[#181b20] p-6 roundered border border-[#2c313a] flex flex-col gap-8">
     <div>
       <h1 class="font-medium text-2xl">Investment Criteria</h1>
       <p class="text-color">Click on the criteria to open a small popup with additional explanations and facts</p>
     </div>
-    <div class="grid grid-cols-3">
-      <div></div>
-      <div></div>
-      <div></div>
+    <div class="grid grid-cols-3 gap-6">
+      <div class="flex flex-col gap-3">
+        <div class="flex gap-3">
+          <div class="round-num">1</div> 
+          <h3>Financial Goals</h3>
+        </div>
+        <Criteria/>
+        <Criteria/>
+        <Criteria/>
+      </div>
+
+      <div class="flex flex-col gap-3">
+        <div class="flex gap-3">
+          <div class="round-num">2</div>
+          <h3>Location Factors</h3>
+        </div>
+        <Criteria/>
+        <Criteria/>
+        <Criteria/>
+      </div>
+
+      <div class="flex flex-col gap-3">
+         <div class="flex gap-3">
+          <div class="round-num">3</div>
+          <h3>Investment Values</h3>
+        </div>
+        <Criteria/>
+        <Criteria/>
+        <Criteria/>
+      </div>
     </div>
   </div>
 </main>
